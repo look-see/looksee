eb8cd45f63e3e20ae9f3a399f18a317d0374c077
allan-mobley-jr <allan.mobley.jr@mobsites.com>
2021-01-03 22:58:19 -0500

    Reset test to pass

--- tests/my-element_test.ts
+++ tests/my-element_test.ts
@@ -4,7 +4,7 @@ import type { MyElement } from 'my-element';
 const testComponent1:MyElement = document.getElementById('test-component-1') as MyElement;
 
 test('#test-component-1 renders with default property values', async () => {
-  testComponent1.name='Allan';
+  // testComponent1.name='Allan';
   console.assert(testComponent1.name === 'World', `Actual: ${testComponent1.name}`);
   await testComponent1.updateComplete;
   console.assert(testComponent1.shadowRoot!.querySelector('h1')!.innerText === 'Hello, World!', `Actual: ${testComponent1.shadowRoot!.querySelector('h1')!.innerText}`);
