0659f1e1230fb2bc4ff303a68886eee282cc17bd
allan-mobley-jr <allan.mobley.jr@mobsites.com>
2021-01-03 20:16:09 -0500

    Removed snowpack dep

--- src/index.ts
+++ src/index.ts
@@ -4,19 +4,15 @@ const yargs = require("yargs");
 const chalk = require('chalk');
 const boxen = require('boxen');
 
-const snowpack = require('./snowpack');
 const messages = require('./messages');
-const puppeteer = require('./puppeteer');
+const puppeteer = require('./watcher');
 
 const options = yargs
- .usage("Usage: -s [path] -o")
- .option("s", { alias: "snowpack", describe: "Path to dev test snowpack.config.js file.", type: "string", demandOption: false })
- .option("o", { alias: "open", describe: "Whether to open browser tab in local development.", type: "boolean", demandOption: false })
- .option("a", { alias: "automation", describe: "Set this flag when running automation scripts in ci/cd pipeline.", type: "boolean", demandOption: false })
+ .usage("Usage: looksee [-w|--watch]")
+ .option("w", { alias: "watch", describe: "Whether to watch for changes in test files.", type: "boolean", demandOption: false })
  .argv;
 
 messages.hello(chalk, boxen);
-snowpack.start(options);
 puppeteer.launch(options, chalk);
 
 process.on('unhandledRejection', function() {
